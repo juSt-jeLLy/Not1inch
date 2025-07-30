90cf8be9c6d039de4ad179fe12860323e9f4967d
anushkasomani <somanianu956@gmail.com>
2025-07-31 01:41:23 +0530

    integration

---
+++ client/frontend/app/providers.tsx
@@ -0,0 +1,27 @@
+'use client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { 
+  SuiClientProvider,
+  WalletProvider,
+  createNetworkConfig 
+} from '@mysten/dapp-kit'
+import { getFullnodeUrl } from '@mysten/sui/client'
+
+const queryClient = new QueryClient()
+
+const { networkConfig } = createNetworkConfig({
+  testnet: { url: getFullnodeUrl('testnet') },
+  mainnet: { url: getFullnodeUrl('mainnet') }
+})
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SuiClientProvider networks={networkConfig} defaultNetwork="testnet">
+        <WalletProvider autoConnect={true}>
+          {children}
+        </WalletProvider>
+      </SuiClientProvider>
+    </QueryClientProvider>
+  )
+}
